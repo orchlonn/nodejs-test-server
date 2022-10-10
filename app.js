9cd89627c79f7d5f14bfd7797719c0348a7d39c5
orchlonn <60744465+orchlonn@users.noreply.github.com>
2022-10-09 17:42:02 -0700

    body parser added

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log("this always runs");
-    next();
-})
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 // add middleware function
 app.use('/add-product', (req, res, next) => {
     console.log("in the another middleware");
-    res.send("<h1>hello from express add product page</h1>")
+    res.send(`<form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Add product</button></form>`);
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 // add middleware function
 app.use('/', (req, res, next) => {
     console.log("in the another middleware");
-    res.send("<h1>hello from express</h1>")
+    res.send("<h1>hello from express</h1>");
 });
 
 app.listen(3000);
